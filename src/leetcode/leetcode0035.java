9e6bae77c15172b5738add75d6ea867a8f09ef58
Fai <nico69274827@gmail.com>
2021-11-15 17:53:35 +0800

    20211115  Solution2

--- src/leetcode/leetcode0035.java
+++ src/leetcode/leetcode0035.java
@@ -15,5 +15,40 @@ public class leetcode0035 {
 		}
 
 	}
+	class Solution2 {
+	    public int searchInsert(int[] nums, int target) {
+	        int head =0 ;
+	        int tail = nums.length-1;
+	        
+
+	if(nums[0]>target) return 0;
+	if(nums[tail]<target) return tail+1;
+
+	       while(head<tail){
+
+	           int ans = head+(tail-head)/2;
+
+	            if(nums[ans]>target){
+	                if(nums[ans-1]<target){
+	                    return ans;
+	                }
+	                tail=ans;
+
+	            }else if(nums[ans]<target){
+	                 if(nums[ans+1]>=target){
+	                    return ans+1;
+	                }
+	                head =ans;
+
+	            }else{
+	                return ans;
+	            }
+
+	       }
+
+
+	        return 0;
+	    }
+	}
 
 }
