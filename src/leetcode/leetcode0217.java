7c30fdd7e7bd3a0267acd6f49774ce4df8494e38
Fai <nico69274827@gmail.com>
2021-11-17 17:56:13 +0800

    20211117

---
+++ src/leetcode/leetcode0217.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class leetcode0217 {
+
+	class Solution {
+		public boolean containsDuplicate(int[] nums) {
+			Arrays.sort(nums);
+			for (int i = 0; i < nums.length - 1; i++) {
+				if (nums[i] == nums[i + 1]) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+
+}
